3269733308db49f7a918d2803232d279daad2a5b
kodi1993 <Akhmedka1993@gmail.com>
2023-01-27 16:05:14 +0300

    Add language, topic modification, amount of news.

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import requests
+from send_email import send_email
 
 api_key = "be8431ec07c348f3a429925736a31fea"
-youtube_api_key = "AIzaSyDdEzmAYt3PfCYYqPN3y0GbuBQCQShfo8g"
-url = f"https://www.youtube.com/playlist?list=PLm1rwsFzd6CBtmBuuLRSBuroTw7" \
-      f"q93vhe{youtube_api_key}"
-url2 = "https://newsapi.org/v2/everything?q=tesla&from=2022-12-25&sortBy=" \
-       "publishedAt&apiKey=be8431ec07c348f3a429925736a31fea"
-# Write a request
-request = requests.get(url2)
-content = request.json()
 
-# Print out content of API
-print(content["articles"][1]["source"]["name"])
+topic = "tesla"
+
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2022-12-27&sortBy" \
+      "=publishedAt&apiKey=be8431ec07c348f3a429925736a31fea&language=en"
+# Write a request
+request = requests.get(url)
+content = request.json()
 print(content)
 
-# Access authors and description of each article
-for cont in content["articles"]:
-    print(f"AUTHOR --- {cont['author']}")
-    print(f"ARTICLE --- {cont['title']}")
+# Send email with title and descriptions
+message = "Subject: Today's News"
+for article in content["articles"][:10]:
+    if article is not None:
+        message += "\n" + article["title"] + "\n" \
+                   + article["description"] + "\n" \
+                   + article["url"] + 2 * "\n"
+
+send_email(message.encode('utf-8'))
+
+
---
+++ send_email.py
@@ -0,0 +1,17 @@
+import os
+import smtplib
+import ssl
+
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    user_name = "Akhmedka1993@gmail.com"
+    password = os.getenv("PASSWORD")
+    recipient = "kodi199312@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, recipient, message)
